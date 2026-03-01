aed68ea141b5040159b0c10d21621509ae641715
LazyTurtle33 <jro0309@gmail.com>
2026-03-01 18:32:49 +0200

    quick fix assets/2X/enhancement.png

--- jokers/TheGreatVote.lua
+++ jokers/TheGreatVote.lua
@@ -6,7 +6,6 @@ SMODS.Joker {
             text = "?????????",
             active = {},
             choices = { -- 5
-                {text = "example", weight = 0, effect = {chips = 0, xchips = 0, mult = 0, xmult = 0, dollars = 0, sell_price_dif = 0, self_destruct = false, joker_copy = {}, extra_slots = {joker = 0, consumeable = 0, hand = 0}, hands_dif = 0, discgard_dif = 0, level_ups = 0, balance = false, staking = true}},
                 {text = "Nothing...",       weight = 0.80, effect = {},                                                 staking = true  },
                 {text = "Self Destruct",    weight = 0.01, effect = {self_destruct = true},                             staking = false },
                 {text = "+100 Chips",       weight = 0.90, effect = {chips = 100},                                      staking = true  },
@@ -32,12 +31,37 @@ SMODS.Joker {
                 {text = "Again!",           weight = 0.03, effect = {repetition = 1},                                   staking = true  },
                 {text = "Negative",         weight = 0.10, effect = {extra_slots = {joker = 1}},                        staking = true  },
                 {text = "Positive",         weight = 0.10, effect = {extra_slots = {joker = -1}},                       staking = true  },
-
-
-
-
-
-
+                
+                
+                
+                
+                
+                
+                {
+                    text = "example",
+                    weight = 0,
+                    effect = {
+                        chips = 0,
+                        xchips = 0,
+                        mult = 0,
+                        xmult = 0,
+                        dollars = 0,
+                        sell_price_dif = 0,
+                        self_destruct = false,
+                        repetition = 0,
+                        joker_copy = {},
+                        extra_slots = {
+                            joker = 0,
+                            consumeable = 0,
+                            hand = 0
+                        }, 
+                        hands_dif = 0,
+                        discgard_dif = 0,
+                        level_ups = 0,
+                        balance = false,
+                    },
+                    staking = true
+                },
             },
     	}
     },
